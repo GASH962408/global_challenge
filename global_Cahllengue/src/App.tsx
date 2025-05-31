afdf69f894830855c71e50b560c74397594696f3
Gustavo Alonso Salazar Huaman <95254724+GASH962408@users.noreply.github.com>
2025-05-31 14:40:34 -0500

    Update App.tsxasdsad

--- global_Cahllengue/src/App.tsx
+++ global_Cahllengue/src/App.tsx
@@ -117,7 +117,7 @@ export default function App() {
         )}
         <h1>Ravn Rick and Morty</h1>
         <h1>Alonso created this </h1>
-        <h1>And this is a new change </h1>
+        <h1>And this is a new change asdasdasd </h1>
       </nav>
       {(!isMobile || showSidebarMobile) && (
         <aside
